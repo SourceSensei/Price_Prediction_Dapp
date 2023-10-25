f8631d6c4ce382773779f92c7e2999b1d7e2acc0
SourceSensei <source.sensei1205@gmail.com>
2023-10-25 01:19:52 +0100

    wallet set up

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,34 @@
 import { useEffect, useMemo, useState } from "react";
+import { RPC_ENDPOINT } from "../utils/constants";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import "@solana/wallet-adapter-react-ui/styles.css";
 import "../styles/globals.css";
+
 function MyApp({ Component, pageProps }) {
+  const [mounted, setMounted] = useState(false);
+
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   return (
-    <Component {...pageProps} />
+    <ConnectionProvider
+      endpoint={RPC_ENDPOINT}
+      config={{ commitment: "confirmed" }}
+    >
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          {mounted && <Component {...pageProps} />}
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 }
 
--- utils/constants.js
+++ utils/constants.js
@@ -1,1 +1,2 @@
-export const RPC_ENDPOINT = ""
+export const RPC_ENDPOINT =
+  "https://solitary-fragrant-feather.solana-devnet.quiknode.pro/8f98a5cdc6a5bed58d526fca63d105ff7dfc2516/";
